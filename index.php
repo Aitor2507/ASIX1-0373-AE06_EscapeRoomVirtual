9813ec0020792ce0e4d02f7deb261ba6d3714326
Aitor2507 <sanchezherenciaaitor@gmail.com>
2025-03-07 10:12:23 +0100

    Agregando favicon + php con inicio corto de formulario

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="img/svg" href="img/puzzle-piece-solid.svg">
+    <title>EscapeRoom - A little problem in my PC</title>
+</head>
+<body>
+    <h2>A little problem in my PC</h2>
+    <form action="" method="post">
+            <p>
+                <input type="text" name="apodo" placebolder="Apodo"> 
+            </p> 
+            <p>
+                <input type="password" name="contrasenya" placebolder="Contraseña"> 
+            </p>
+            <p>
+                <input type="submit" value="Entrar"> 
+            </p>
+        </form>
+    </body>
+</html>
+</body>
+</html>
