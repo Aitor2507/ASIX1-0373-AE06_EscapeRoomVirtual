b57ac870849a97b1f9ea30a88a95e66261ef8e6b
Aitor2507 <sanchezherenciaaitor@gmail.com>
2025-03-09 18:22:01 +0100

    Arreglo de inicio con submit y CSS de este. Estrucutra completa

--- index.php
+++ index.php
@@ -1,19 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="img/svg" href="img/puzzle-piece-solid.svg">
     <link rel="stylesheet" href="css/styles.css">
-    <title>EscapeRoom - A little problem in my PC</title>
+    <title>EscapeRoom - Un pequeño problema en mi PC</title>
+    
 </head>
+
 <body>
-    <h2>A little problem in my PC</h2>
+    <div class="matrix-container"></div>
+    <h2>Un pequeño problema en mi PC</h2>
+    <table border="1px">
+    <p>Eres un analista de ciberseguridad trabajando en tu computadora como cualquier otro día… hasta que algo extraño sucede. De repente, la pantalla se llena de código corrupto, la interfaz cambia y un mensaje aparece:
+
+        "SISTEMA COMPROMETIDO. ACCESO DENEGADO. INTRODUCE LAS CREDENCIALES CORRECTAS PARA RECUPERAR EL CONTROL."
+
+        Parece que alguien ha activado un bloqueo en tu sistema. Si no resuelves los acertijos a tiempo, tu equipo quedará inutilizado y todos tus archivos desaparecerán para siempre.
+
+        Tu única opción es navegar por los cuatro niveles de seguridad del sistema y descifrar los códigos correctos. Pero cuidado: si fallas, serás expulsado al nivel anterior y el sistema se volverá más inestable.
+
+        ¿Tienes las habilidades necesarias para hackear tu propio sistema y recuperar el control antes de que sea demasiado tarde?
+
+        El tiempo corre... 
+    </p>
+    </table>
     <div class="style_form">
     <form action="proc/res.proc.php" method="POST">
-            <label></label>
+            <a href="">Iniciar el reto</label>
     </form>
     </div>
+    <script src="bg_index.js"></script>
     </body>
 </html>
 </body>
